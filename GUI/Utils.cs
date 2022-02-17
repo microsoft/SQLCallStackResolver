6aefa5de32ac7390d0ed016103da166f6a8e9f32
Arvind Shyamsundar <arvindsh@microsoft.com>
2022-02-16 18:33:35 -0800

    Add notification for new releases

---
+++ GUI/Utils.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License - see LICENSE file in this repo.
+namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
+    using System;
+    using System.IO;
+    using System.Net;
+
+    internal class Utils {
+        internal static string GetFileContentsFromUrl(string url) {
+            var httpReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
+
+            try {
+                HttpWebResponse httpResp = (HttpWebResponse)httpReq.GetResponse();
+                if (httpResp != null) {
+                    if (httpResp.StatusCode == HttpStatusCode.OK) {
+                        using (var strm = new StreamReader(httpResp.GetResponseStream()))
+                            return strm.ReadToEnd().Trim();
+                    }
+                }
+            } catch (WebException) {
+            }
+
+            return null;
+        }
+    }
+}
