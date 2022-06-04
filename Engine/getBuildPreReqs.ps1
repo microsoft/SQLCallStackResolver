387236a4c287cd220093e7389f872d2a779e4f04
Arvind Shyamsundar <arvindsh@microsoft.com>
2022-06-04 07:06:01 -0700

    Use Microsoft.TestPlatform NuGet for referencing DIA

--- Engine/getBuildPreReqs.ps1
+++ Engine/getBuildPreReqs.ps1
@@ -1,42 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License - see LICENSE file in this repo.
 
-# Find out the installation path for the highest version of Visual Studio installed
-$vsMaxVerFound = 0
-$vsPath = $null
-$vsVersions = (& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -legacy -prerelease -format json) | ConvertFrom-Json
-foreach ($vsInstance in $vsVersions) {
-    $vsFullVersion = $vsInstance.installationVersion
-    $verFound = [int]::Parse($vsFullVersion.Split(".")[0])
-    if ($verFound -gt $vsMaxVerFound) {
-        $vsMaxVerFound = $verFound
-        $vsPath = $vsInstance.installationPath
-    }    
-}
-
-if ($null -eq $vsPath) {
-    Write-Error "No Visual Studio installation found. Exiting..." -ErrorAction Stop    
-}
-
-Write-Host "Using Visual Studio $vsFullVersion from $vsPath"
-
-if (-not (Test-Path "./DIA")) {
-    mkdir DIA
-}
-
-copy "$vsPath\Team Tools\Performance Tools\x64\msdia140.dll" "./DIA/msdia140.dll"
-copy "$vsPath\Team Tools\Performance Tools\x64\msdia140.dll.manifest" "./DIA/msdia140.dll.manifest"
-
-Write-Host
 Write-Host "DIA file versions:"
-$diaFiles = dir "./DIA/*.dll"
+$diaFiles = @(dir "../packages/Microsoft.TestPlatform.*/tools/net451/Common7/IDE/Extensions/TestPlatform/x64/msdia140.dll")
 foreach ($file in $diaFiles) {
     Write-Host $file.FullName":" $file.VersionInfo.FileVersion
 }
 
-if ((Get-ChildItem ".\DIA\*.*").Length -ne 2) {
+if ((dir "../packages/Microsoft.TestPlatform.*/tools/net451/Common7/IDE/Extensions/TestPlatform/x64/msdia140.*").Length -ne 2) {
     Write-Error "You must manually obtain msdia140.dll and msdia140.dll.manifest, and copy them to the Engine\DIA sub-folder. Those are redistributable components of Visual Studio 2022 subject to terms as published here: https://docs.microsoft.com/en-us/visualstudio/releases/2022/redistribution." -ErrorAction Stop
 }
+
+$diaManifestPath = "../packages/Microsoft.TestPlatform.*/tools/net451/Common7/IDE/Extensions/TestPlatform/x64/msdia140.dll.manifest"
+# fix the file path to msdia140.dll within the manifest
+(Get-Content $diaManifestPath).Replace("ComComponents\x64\msdia140.dll", "msdia140.dll") | Set-Content $diaManifestPath
 
 Write-Host "Debugger file versions:"
 $dbgFiles = @(dir "../packages/Microsoft.Debugging.Platform.DbgEng.*/content/amd64/dbghelp.dll")
