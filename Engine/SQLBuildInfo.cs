0d100c5322c100f37868fc09123350169df39f7d
Arvind Shyamsundar <arvindsh@microsoft.com>
2022-06-10 13:17:06 -0700

    Use built-in library for JSON handling (#62)

--- Engine/SQLBuildInfo.cs
+++ Engine/SQLBuildInfo.cs
@@ -1,21 +1,30 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License - see LICENSE file in this repo.
 namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
-    using Newtonsoft.Json;
     using System.Collections.Generic;
     using System.Diagnostics.Contracts;
     using System.Globalization;
     using System.IO;
     using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
     using System.Text;
 
+    [DataContract]
     public class SQLBuildInfo {
+        [DataMember(Order=0)]
         public string ProductMajorVersion = "<<ProductMajorVersion>>";
+        [DataMember(Order = 1)]
         public string ProductLevel = "<<ProductLevel>>";
+        [DataMember(Order = 2)]
         public string Label = "<<BuildName>>";
+        [DataMember(Order = 3)]
         public string BuildNumber = "<<BuildNumber>>";
+        [DataMember(Order = 4)]
         public string KBInfo = "<<KBArticle>>";
+        [DataMember(Order = 5)]
         public List<Symbol> SymbolDetails;
+        [DataMember(Order = 6)]
         public string MachineType = "<<x64|x86>>";
 
         public override string ToString() {
@@ -27,37 +36,29 @@ namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
             var allBuilds = new SortedDictionary<string, SQLBuildInfo>();
 
             using (var fs = new FileStream(jsonFile, FileMode.Open, FileAccess.Read, FileShare.None)) {
-                using (var rdr = new StreamReader(fs)) {
-                    using (var jsonRdr = new JsonTextReader(rdr)) {
-                        jsonRdr.SupportMultipleContent = true;
-                        var serializer = new JsonSerializer();
-                        while (true) {
-                            if (!jsonRdr.Read()) {
-                                break;
-                            }
+                using (var sr = new StreamReader(fs)) {
+                    var allLines = sr.ReadToEnd().Split('\n');
+                    var jsonSerializer = new DataContractJsonSerializer(typeof(SQLBuildInfo));
+                    foreach (var line in allLines.Where(l => !string.IsNullOrEmpty(l))) {
+                        using (var memStream = new MemoryStream(Encoding.UTF8.GetBytes(line))) {
+                            var currBuildInfo = jsonSerializer.ReadObject(memStream) as SQLBuildInfo;
+                            if (currBuildInfo != null) {
+                                currBuildInfo.BuildNumber = currBuildInfo.BuildNumber.Trim();
+                                currBuildInfo.KBInfo = currBuildInfo.KBInfo.Trim();
+                                currBuildInfo.Label = currBuildInfo.Label.Trim();
+                                currBuildInfo.ProductLevel = currBuildInfo.ProductLevel.Trim();
+                                currBuildInfo.ProductMajorVersion = currBuildInfo.ProductMajorVersion.Trim();
 
-                            var currBuildInfo = serializer.Deserialize<SQLBuildInfo>(jsonRdr);
-                            currBuildInfo.BuildNumber = currBuildInfo.BuildNumber.Trim();
-                            currBuildInfo.KBInfo = currBuildInfo.KBInfo.Trim();
-                            currBuildInfo.Label = currBuildInfo.Label.Trim();
-                            currBuildInfo.ProductLevel = currBuildInfo.ProductLevel.Trim();
-                            currBuildInfo.ProductMajorVersion = currBuildInfo.ProductMajorVersion.Trim();
-
-                            if (!allBuilds.ContainsKey(currBuildInfo.ToString())) {
-                                allBuilds.Add(currBuildInfo.ToString(), currBuildInfo);
-                            }
-                            else {
-                                allBuilds[currBuildInfo.ToString()] = currBuildInfo;
+                                if (!allBuilds.ContainsKey(currBuildInfo.ToString())) {
+                                    allBuilds.Add(currBuildInfo.ToString(), currBuildInfo);
+                                } else {
+                                    allBuilds[currBuildInfo.ToString()] = currBuildInfo;
+                                }
                             }
                         }
-
-                        jsonRdr.Close();
                     }
-
-                    rdr.Close();
                 }
             }
-
             return new SortedDictionary<string, SQLBuildInfo>(allBuilds);
         }
 
@@ -66,15 +67,13 @@ namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
                 using (var fs = new FileStream(jsonFile, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None)) {
                     // initially, truncate the file
                     fs.SetLength(0);
-
-                    using (var wrtr = new StreamWriter(fs)) {
-                        foreach (var bld in allBuilds) {
-                            wrtr.WriteLine(JsonConvert.SerializeObject(bld));
-                        }
-
-                        wrtr.Flush();
-                        wrtr.Close();
+                    var jsonSerializer = new DataContractJsonSerializer(typeof(SQLBuildInfo));
+                    foreach (var bld in allBuilds) {
+                        jsonSerializer.WriteObject(fs, bld);
+                        fs.Write(new byte[] { 13, 10 }, 0, 2);   // \r\n after each JSON "record"
                     }
+                    fs.Flush();
+                    fs.Close();
                 }
             }
         }
