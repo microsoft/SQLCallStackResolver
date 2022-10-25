ed84aa617303bb5db54266a3ce1f45294e882358
Arvind Shyamsundar <arvindsh@microsoft.com>
2022-10-24 22:44:03 -0700

    Eliminate avoidable SymSrv init / cleanup (#98)

--- Engine/SymSrvHelpers.cs
+++ Engine/SymSrvHelpers.cs
@@ -2,27 +2,15 @@
 // Licensed under the MIT License - see LICENSE file in this repo.
 namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
     public static class SymSrvHelpers {
-        static readonly int processId = Process.GetCurrentProcess().Id;
-
-        /// Wrapper around the symsrv.dll functionality to initialize the symbol load handler for this process.
-        private static bool InitSymSrv(string symPath) {
-            return SafeNativeMethods.SymInitialize((IntPtr)processId, symPath, false);
-        }
-
-        /// Un-initialize the symbol load handler for this process.
-        private static bool CleanupSymSrv() {
-            return SafeNativeMethods.SymCleanup((IntPtr)processId);
-        }
-
         /// Private method to locate the local path for a matching PDB. Implicitly handles symbol download if needed.
-        private static string GetLocalSymbolFolderForModule(string pdbFilename, string pdbGuid, int pdbAge) {
+        private static string GetLocalSymbolFolderForModule(string pdbFilename, string pdbGuid, int pdbAge, string symPath) {
             const int MAX_PATH = 4096;
             StringBuilder outPath = new(MAX_PATH);
             var guid = Guid.Parse(pdbGuid);
             int rawsize = Marshal.SizeOf(guid);
             IntPtr buffer = Marshal.AllocHGlobal(rawsize);
             Marshal.StructureToPtr(guid, buffer, false);
-            bool success = SafeNativeMethods.SymFindFileInPath((IntPtr)processId, null, pdbFilename, buffer, pdbAge, 0, 8, outPath, IntPtr.Zero, IntPtr.Zero);
+            bool success = SafeNativeMethods.SymFindFileInPath(IntPtr.Zero, symPath, pdbFilename, buffer, pdbAge, 0, 8, outPath, IntPtr.Zero, IntPtr.Zero);
             if (!success)  return String.Empty;
             return outPath.ToString();
         }
@@ -34,11 +22,10 @@ namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
             var retval = new List<string>();
             Contract.Requires(null != syms);
             Contract.Requires(null != parent);
-            if (!InitSymSrv(symPath)) return retval;
             int progress = 0;
             foreach (var sym in syms) {
                 parent.StatusMessage = string.Format(CultureInfo.CurrentCulture, $"Finding local PDB path for {sym.PDBName}");
-                var path = GetLocalSymbolFolderForModule(sym.PDBName, sym.PDBGuid, sym.PDBAge);
+                var path = GetLocalSymbolFolderForModule(sym.PDBName, sym.PDBGuid, sym.PDBAge, symPath);
                 if (!string.IsNullOrEmpty(path)) {
                     retval.Add(Path.GetDirectoryName(path));
                     parent.StatusMessage = string.Format(CultureInfo.CurrentCulture, $"Successfully found local PDB at {path}");
@@ -48,8 +35,6 @@ namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver {
                 progress++;
                 parent.PercentComplete = (int)((double)progress / syms.Count * 100.0);
             }
-
-            CleanupSymSrv();
             return retval;
         }
     }
