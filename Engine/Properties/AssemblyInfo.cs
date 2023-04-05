b3804231f61808ca06983c41f9d3d612947897a1
Arvind Shyamsundar <arvindsh@microsoft.com>
2023-04-04 21:19:02 -0700

    Prompt user when resolving clipboard contents
    * Add a prompt to confirm if user wants to resolve clipboard contents.
    * Optionally remember the user's choice.
    * Update README and supporting images.
    * Update assembly versions to 2.3.0.0.
    * Update the latest release date.

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("782bbd60-ee45-43cd-8a4f-0505efe4ff1a")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 [assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.7.2", FrameworkDisplayName = ".NET Framework 4.7.2")]
---
+++ GUI/Properties/Settings.Designer.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.SqlServer.Utils.Misc.SQLCallStackResolver.Properties {
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()][global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()][global::System.Diagnostics.DebuggerNonUserCodeAttribute()][global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool promptForClipboardPaste {
+            get {
+                return ((bool)(this["promptForClipboardPaste"]));
+            }
+            set {
+                this["promptForClipboardPaste"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()][global::System.Diagnostics.DebuggerNonUserCodeAttribute()][global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool choiceForClipboardPaste {
+            get {
+                return ((bool)(this["choiceForClipboardPaste"]));
+            }
+            set {
+                this["choiceForClipboardPaste"] = value;
+            }
+        }
+    }
+}
