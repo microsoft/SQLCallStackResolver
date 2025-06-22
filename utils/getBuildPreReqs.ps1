d351dbc5dd7d98bf47874ac4c6a1c7cf02db1c66
Arvind Shyamsundar <16342666+arvindshmicrosoft@users.noreply.github.com>
2025-06-21 22:07:34 -0700

    Release 20250622 (#146)
    * Update debugger nugets to 20250611.1057.0
    * Set latest release to 2025-06-22

--- utils/getBuildPreReqs.ps1
+++ utils/getBuildPreReqs.ps1
@@ -19,5 +19,5 @@ if ((dir "DIA/*").Length -ne 3)
 $diaManifestPath = "DIA/msdia140.dll.manifest"
 (Get-Content $diaManifestPath).Replace("DIA/msdia140.dll", "msdia140.dll") -Replace " description", " threadingModel=`"Both`" description " | Set-Content $diaManifestPath
 
-@(dir "../packages/Microsoft.Debugging.Platform.DbgEng.20250411.1210.0/content/amd64/dbghelp.dll").VersionInfo.ToString()
-@(dir "../packages/Microsoft.Debugging.Platform.SymSrv.20250411.1210.0/content/amd64/symsrv.dll").VersionInfo.ToString()
+@(dir "../packages/Microsoft.Debugging.Platform.DbgEng.20250611.1057.0/content/amd64/dbghelp.dll").VersionInfo.ToString()
+@(dir "../packages/Microsoft.Debugging.Platform.SymSrv.20250611.1057.0/content/amd64/symsrv.dll").VersionInfo.ToString()
